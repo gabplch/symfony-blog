05fab851a062517eac43b4cd4bc5df106d1a2ad4
Yarysh Vlad <yarysh.vlad@gmail.com>
2022-09-30 00:04:16 +0300

    Login form & operation realized

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'security_login')]
+    public function login(AuthenticationUtils $authHelper): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('blog_index');
+        }
+
+        return $this->render('security/login.html.twig', [
+            'username' => $authHelper->getLastUsername(),
+            'error' => $authHelper->getLastAuthenticationError(),
+        ]);
+    }
+}
