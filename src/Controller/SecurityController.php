8b34f61686034b293296c78e89693aedeaca3669
Yarysh Vlad <yarysh.vlad@gmail.com>
2022-09-30 01:17:00 +0300

    Registration add

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {
+    }
+
+    #[Route('/register', name: 'security_registration')]
+    public function registration(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        if ($user) {
+            return $this->redirectToRoute('blog_index');
+        }
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+
+            $this->userRepository->save($user, true);
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     #[Route('/login', name: 'security_login')]
     public function login(AuthenticationUtils $authHelper): Response
     {
